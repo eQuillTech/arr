d826318974b06463d00ea48e754f82a29d992372
Phil Ahrenkiel <61021639+spahrenk@users.noreply.github.com>
2025-11-27 13:04:56 -0700

    Add ObjC interface for iOS.

---
+++ arr.h
@@ -0,0 +1,12 @@
+//
+//  arr.h
+//  arr
+//
+//  Created by Phil Ahrenkiel on 11/27/25.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface arr : NSObject
+
+@end
---
+++ arr.m
@@ -0,0 +1,12 @@
+//
+//  arr.m
+//  arr
+//
+//  Created by Phil Ahrenkiel on 11/27/25.
+//
+
+#import "arr.h"
+
+@implementation arr
+
+@end
